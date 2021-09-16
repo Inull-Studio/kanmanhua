2a390be18a4dea01fb9d4643ad83a08032ed2ca9
basket_ball <2293830442@qq.com>
2021-09-16 21:06:18 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,37 +4,34 @@ from requests import get
 from bs4 import BeautifulSoup
 from urllib.parse import unquote
 from json import loads
-import cv2
 from os import chdir, mkdir, getcwd, path
 from tempfile import mktemp, gettempdir
 from shutil import move
+from threading import Thread
 
 from sys import argv
 
 
-SRC = getcwd()
-SEARCH_API = 'https://www.kanman.com/api/getsortlist'
-CHAPTERINFO_API = 'https://www.kanman.com/api/getchapterinfov2'
-KANMANHUA = 'https://www.kanman.com'
-HEADER = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.864.41'}
-
-
 class KanManHua():
-    def __init__(self):
+    def __init__(self, proxy=False):
         super(KanManHua, self).__init__()
-
         self.SRC = getcwd()
         self.SEARCH_API = 'https://www.kanman.com/api/getsortlist'
         self.CHAPTERINFO_API = 'https://www.kanman.com/api/getchapterinfov2'
         self.KANMANHUA = 'https://www.kanman.com'
         self.HEADER = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.864.41'}
+        if proxy:
+            self.proxies = {'http': 'http://127.0.0.1:2802'}
+            print('proxy')
+        else:
+            print('not proxy')
+            self.proxies = {}
 
     def _search(self, keyword):
         try:
             r = get(
-                self.SEARCH_API, params={'search_key': keyword}, headers=self.HEADER)
+                self.SEARCH_API, params={'search_key': keyword}, headers=self.HEADER, proxies=self.proxies)
             res = r.json()
             if res['message'] == 'ok':
                 res = r.json()['data']
@@ -55,7 +52,8 @@ class KanManHua():
             print(e)
 
     def _get_first_chapter_id(self):
-        r = get(self.KANMANHUA+f'/{self.comic_id}', headers=self.HEADER)
+        r = get(self.KANMANHUA+f'/{self.comic_id}',
+                headers=self.HEADER, proxies=self.proxies)
         if r.status_code == 200:
             r.encoding = 'utf8'
             soup = BeautifulSoup(r.text, 'lxml')
@@ -64,8 +62,8 @@ class KanManHua():
             print('获取chapterid成功')
 
     def _chapter_info(self, comic_id, chapter_newid: str):
-        r = get(CHAPTERINFO_API, params={
-                'comic_id': comic_id, 'chapter_newid': chapter_newid, 'quality': 'middle'}, headers=HEADER)
+        r = get(self.CHAPTERINFO_API, params={
+                'comic_id': comic_id, 'chapter_newid': chapter_newid, 'quality': 'middle'}, headers=self.HEADER, proxies=self.proxies)
         r.encoding = 'utf8'
         res = r.text
         res = unquote(res)
@@ -82,23 +80,33 @@ class KanManHua():
         self._chapter_info(self.comic_id, chapter_newid)
 
     def _get_imgs(self):
+        if not path.exists('downloads'):
+            mkdir('downloads')
+        self.comic_name = self.chapter_info['data']['comic_name']
+        sets = ['/', '\\', ':', '*', '?', '"', '<', '>', '|', '.']
         chapter_name = self.chapter_info['data']['current_chapter']['chapter_name']
-        if not path.exists(path.join(self.SRC, 'downloads', chapter_name)):
-            mkdir(path.join(self.SRC, 'downloads', chapter_name))
+        for char in chapter_name:
+            if char in sets:
+                chapter_name = chapter_name.replace(char, '')
+        if not path.exists(path.join(self.SRC, 'downloads', self.comic_name, chapter_name)):
+            if not path.exists(path.join(self.SRC, 'downloads', self.comic_name)):
+                mkdir(path.join(self.SRC, 'downloads', self.comic_name))
+            mkdir(path.join(self.SRC, 'downloads', self.comic_name, chapter_name))
         self.images = self.chapter_info['data']['current_chapter']['chapter_img_list'], chapter_name
 
     def _download(self, img_url: str, chapter_name: str):
         try:
-            if not path.exists('downloads'):
-                mkdir('downloads')
-            r = get(img_url, headers=HEADER)
-            print('正在下载:', chapter_name, img_url.split('/')[-1].split('-')[0])
+            if path.exists(path.join(self.SRC, 'downloads', self.comic_name, chapter_name, img_url.split('/')[-1].split('-')[0])):
+                return
+            r = get(img_url, headers=self.HEADER, proxies=self.proxies)
+            print(
+                '正在下载:', self.comic_name, chapter_name, img_url.split('/')[-1].split('-')[0])
             filename = mktemp()
             f = open(filename, 'wb')
             f.write(r.content)
             f.close()
             move(filename, path.join(
-                SRC, 'downloads', chapter_name, img_url.split('/')[-1].split('-')[0]))
+                self.SRC, 'downloads', self.comic_name, chapter_name, img_url.split('/')[-1].split('-')[0]))
         except Exception as e:
             print(e)
 
@@ -107,15 +115,27 @@ class KanManHua():
 
 
 def main():
-    kanman = KanManHua()
-    kanman._search('妖神')
-    kanman._get_first_chapter_id()
-    kanman._chapter_info(kanman.comic_id, kanman.chapter_newid)
-    while kanman._is_next_chapter():
-        kanman._get_imgs()
-        for img in kanman.images[0]:
-            kanman._download(img, kanman.images[1])
-        kanman._next_chapter_info()
+    try:
+        kanman = KanManHua(proxy=False)
+        kanman._search('妖神')
+        kanman._get_first_chapter_id()
+        kanman._chapter_info(kanman.comic_id, kanman.chapter_newid)
+        while kanman._is_next_chapter():
+            tlist = []
+            kanman._get_imgs()
+            for img in kanman.images[0]:
+                t = Thread(
+                    target=kanman._download, args=(img, kanman.images[1]))
+                t.setDaemon(True)
+                tlist.append(t)
+                # kanman._download(img, kanman.images[1])
+            for t in tlist:
+                t.start()
+            for t in tlist:
+                t.join()
+            kanman._next_chapter_info()
+    except KeyboardInterrupt:
+        exit(0)
 
 
 if __name__ == '__main__':
